025d5a9e6af7ac1739c38fae7924ed84d8a0b501
Franklin Ross <fhross@gmail.com>
2019-12-08 16:49:04 +1100

    Add toShellArgs
    For getting an array of strings to pass to a shell.

--- dist/tokens/List.d.ts
+++ dist/tokens/List.d.ts
@@ -1,6 +1,6 @@
-import { TokenKind, Variable, QuotedString, VerbatimString, Word, Whitespace, SubstitutedVariable } from ".";
+import { TokenKind, Variable, QuotedString, VerbatimString, Word, Whitespace, SubstitutedVariable, VariableAssignment } from ".";
 /** The types of token allowed in a List. */
-export declare type ListItem = Variable | SubstitutedVariable | Word | QuotedString | VerbatimString | Whitespace;
+export declare type ListItem = Variable | SubstitutedVariable | VariableAssignment | Word | QuotedString | VerbatimString | Whitespace;
 /** A list of tokens, including whitespace. Handles most of the rules for collapsing whitespace
  * based on context. */
 export declare class List {
---
+++ test/transforms/toShellArgs.test.js
@@ -0,0 +1,63 @@
+const {
+  toShellArgs,
+  SubstitutedVariable: SubVar,
+  Variable: Var,
+  VariableAssignment: VarAssign,
+  Word: W,
+  List: L,
+  QuotedString: QS,
+  VerbatimString: VS,
+  Whitespace: Ws
+} = require("../../");
+
+describe("transforms.toShellArgs", () => {
+  const tests = [
+    [new L([new W("a"), new Ws(" "), new W("b")]), ["a", "b"]],
+    [
+      new L([
+        new W("a"),
+        new Ws(" "),
+        new SubVar("V", null),
+        new Ws(" "),
+        new W("b")
+      ]),
+      ["a", "b"]
+    ],
+    [
+      new L([
+        new W("a"),
+        new Ws(" "),
+        new SubVar("V", new L([new SubVar("X", new W("c"))])),
+        new Ws(" "),
+        new W("b")
+      ]),
+      ["a", "c", "b"]
+    ],
+    [
+      new L([
+        new W("a"),
+        new Ws(" "),
+        new Var("V", ":-", new L([new W("c")])),
+        new Ws(" "),
+        new W("a")
+      ]),
+      ["a", "b"]
+    ],
+    [new L([new QS(["a"]), new Ws(" "), new VS("b")]), ["a", "b"]],
+    [
+      new L([
+        new VarAssign("V", new L([new QS("x")])),
+        new Ws(" "),
+        new W("b")
+      ]),
+      ["V=x", "b"] // I don't know whether variable assignments should show up.
+    ]
+  ];
+
+  tests.forEach(([ast, result]) =>
+    it(ast.toString(), () => {
+      const args = toShellArgs(ast);
+      expect(args).toEqual(result);
+    })
+  );
+});
